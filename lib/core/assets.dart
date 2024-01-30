5bb5fed38791e1d3f4dbe30b1915e23e3e96b930
Mahdi <mti.safdari@gmail.com>
2024-01-30 14:47:30 +0330

    feat: add assets to dart

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -1,3 +1,24 @@
-class R {
-  static const Background = 'assets/images/background.png';
+class Assets {
+  static const String background = 'assets/images/background.png';
+  static const String house = 'assets/images/House.png';
+  static const String hover = 'assets/images/Hover.svg';
+  static const String button = 'assets/images/Button.svg';
+  static const String rectangle = 'assets/images/Rectangle 364.svg';
+  static const String subtract = 'assets/images/Subtract.svg';
+  static const String listIcon = 'assets/images/List.svg';
+  static const String ellipse1 = 'assets/images/Ellipse1.png';
+  static const String ellipse2 = 'assets/images/Ellipse2.png';
+  static const String ellipse3 = 'assets/images/Ellipse3.png';
+  static const String separator = 'assets/images/Separator.png';
+  static const String underline = 'assets/images/Underline.png';
+  static const String weatherIcon = 'assets/images/Weather-Icon.png';
+  static const String chevronLeft = 'assets/images/chevron.left.svg';
+  static const String rightTitle = 'assets/images/Right-Title.svg';
+  static const String regtangle1 = 'assets/images/Rectangle1.svg';
+  static const String moonCloudMidRain = 'assets/images/Moon cloud mid rain.png';
+  static const String ellipse5 = 'assets/images/Ellipse 5.png';
+  static const String ellipse4 = 'assets/images/Ellipse 4.png';
+  static const String uvIndex = 'assets/images/uv-index.png';
+  static const String wind = 'assets/images/wind.svg';
+  static const String currentLocation = 'assets/images/undraw_my_current_location_re_whmt.svg';
 }
