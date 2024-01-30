d746c5a9d6633bb80387098071f213906638d2a5
Mahdi <mti.safdari@gmail.com>
2024-01-30 15:15:37 +0330

    feat: constants created

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const String baseUrl = 'https://api.openweathermap.org';
+  static const String apiKey = '4b2e1f18f1004cbd6ef8a9d05f204a42';
+  static const String apiKey2 = 'bd5e378503939ddaee76f12ad7a97608';
+  static const Color primary = Color(0xffFFFFFF);
+  static final Color secondary = const Color(0xffEBEBF5).withOpacity(0.6);
+  static final Color tertiary = const Color(0xffEBEBF5).withOpacity(0.3);
+  static final Color quaternary = const Color(0xffEBEBF5).withOpacity(0.18);
+  static const Color scaffoldBackgroundColor = Color(0xff3A3F54);
+  static final Color indicator = const Color(0xff000000).withOpacity(0.4);
+  static const Color underline = Color(0xffDA99DD);
+  static const Color percentRainColor = Color(0xFF40CBD8);
+  static const Color backgroundColor = Color(0xff2E335A);
+  static const Color selectItemColor = Color(0xff48319D);
+  static const Color shadowColor = Color(0x3F000000);
+  static final Color itemColor = const Color(0xff24234E).withOpacity(0.4);
+}
