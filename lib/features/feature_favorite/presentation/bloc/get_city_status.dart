d12655e41ce748bfa90cf45bda9d5ddf59045a81
Mahdi <mti.safdari@gmail.com>
2024-02-20 16:00:53 +0330

    feat(favorite): create get city status

--- lib/features/feature_favorite/presentation/bloc/get_city_status.dart
+++ lib/features/feature_favorite/presentation/bloc/get_city_status.dart
@@ -1,27 +1,27 @@
 import 'package:breezehub/features/feature_favorite/domain/entities/city_entity.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class FindCityStatus extends Equatable {}
+abstract class GetCityStatus extends Equatable {}
 
-class FindCityLoading extends FindCityStatus {
+class GetCityLoading extends GetCityStatus {
   @override
   // TODO: implement props
   List<Object?> get props => [];
 }
 
-class FindCityCompleted extends FindCityStatus {
+class GetCityCompleted extends GetCityStatus {
   final CityEntity? cityEntity;
 
-  FindCityCompleted(this.cityEntity);
+  GetCityCompleted(this.cityEntity);
   @override
   // TODO: implement props
   List<Object?> get props => [cityEntity];
 }
 
-class FindCityError extends FindCityStatus {
+class GetCityError extends GetCityStatus {
   final String message;
 
-  FindCityError(this.message);
+  GetCityError(this.message);
   @override
   // TODO: implement props
   List<Object?> get props => [message];
