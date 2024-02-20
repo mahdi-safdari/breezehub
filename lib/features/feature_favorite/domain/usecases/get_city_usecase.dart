9edfc22c74c20e4aadb9953afc9029851866ed52
Mahdi <mti.safdari@gmail.com>
2024-02-20 15:56:18 +0330

    feat(domain layer): create get city usecase

--- lib/features/feature_favorite/domain/usecases/get_city_usecase.dart
+++ lib/features/feature_favorite/domain/usecases/get_city_usecase.dart
@@ -3,9 +3,9 @@ import 'package:breezehub/core/usecases/use_case.dart';
 import 'package:breezehub/features/feature_favorite/domain/entities/city_entity.dart';
 import 'package:breezehub/features/feature_favorite/domain/repository/city_repository.dart';
 
-class FindCityUseCase extends UseCase<DataState<CityEntity?>, String> {
+class GetCityUseCase extends UseCase<DataState<CityEntity?>, String> {
   final CityRepository cityRepository;
-  FindCityUseCase(this.cityRepository);
+  GetCityUseCase(this.cityRepository);
 
   @override
   Future<DataState<CityEntity?>> call(String param) {
