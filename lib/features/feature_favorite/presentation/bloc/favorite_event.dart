46875b64a9e9bbd3b4f9f7abe727e76434a016ac
Mahdi <mti.safdari@gmail.com>
2024-02-20 15:59:30 +0330

    feat(favorite): create event

--- lib/features/feature_favorite/presentation/bloc/favorite_event.dart
+++ lib/features/feature_favorite/presentation/bloc/favorite_event.dart
@@ -2,3 +2,25 @@ part of 'favorite_bloc.dart';
 
 @immutable
 abstract class FavoriteEvent {}
+
+class GetAllCityEvent extends FavoriteEvent {}
+
+class GetCityEvent extends FavoriteEvent {
+  final String cityName;
+
+  GetCityEvent(this.cityName);
+}
+
+class SaveCityEvent extends FavoriteEvent {
+  final CityEntity cityEntity;
+
+  SaveCityEvent(this.cityEntity);
+}
+
+class DeleteCityEvent extends FavoriteEvent {
+  final String cityName;
+
+  DeleteCityEvent(this.cityName);
+}
+
+class SaveCityInitialEvent extends FavoriteEvent {}
