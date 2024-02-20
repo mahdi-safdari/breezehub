c0e8b19c9804f79c813b29f783e9d5f910643259
Mahdi <mti.safdari@gmail.com>
2024-02-20 16:23:42 +0330

    feat(weather): create get main data usecase

--- lib/features/feature_weather/domain/usecases/get_main_data_usecase.dart
+++ lib/features/feature_weather/domain/usecases/get_main_data_usecase.dart
@@ -1,16 +1,16 @@
-import 'package:breezehub/core/params/params.dart';
+import 'package:breezehub/core/params/location_params.dart';
 import 'package:breezehub/core/resources/data_state.dart';
 import 'package:breezehub/core/usecases/use_case.dart';
 import 'package:breezehub/features/feature_weather/domain/entities/current_city_entity.dart';
 import 'package:breezehub/features/feature_weather/domain/repositories/weather_repository.dart';
 
-class GetWeatherMainDataUseCase extends UseCase<DataState<CurrentCityEntity>, LocationParams> {
+class GetMainDataUseCase extends UseCase<DataState<CurrentCityEntity>, LocationParams> {
   final WeatherRepository weatherRepository;
 
-  GetWeatherMainDataUseCase(this.weatherRepository);
+  GetMainDataUseCase(this.weatherRepository);
 
   @override
   Future<DataState<CurrentCityEntity>> call(LocationParams param) {
-    return weatherRepository.getWeatherMainData(param);
+    return weatherRepository.getMainData(param);
   }
 }
