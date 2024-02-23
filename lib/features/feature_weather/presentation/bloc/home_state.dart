ce4a1be26861190c6b0055ed06117d5f543ce084
Mahdi <mti.safdari@gmail.com>
2024-02-23 07:38:16 +0330

    feat(weather): create home state

--- lib/features/feature_weather/presentation/bloc/home_state.dart
+++ lib/features/feature_weather/presentation/bloc/home_state.dart
@@ -1,10 +1,37 @@
 part of 'home_bloc.dart';
 
-abstract class HomeState extends Equatable {
-  const HomeState();
-}
+class HomeState extends Equatable {
+  final CurrentWeatherStatus currentWeatherStatus;
+  final ForecastDaysStatus forecastDaysStatus;
+  final WeatherInformationStatus informationStatus;
+  final MainDataStatus mainDataStatus;
 
-class HomeInitial extends HomeState {
+  const HomeState({
+    required this.mainDataStatus,
+    required this.informationStatus,
+    required this.forecastDaysStatus,
+    required this.currentWeatherStatus,
+  });
+
+  HomeState copyWith({
+    CurrentWeatherStatus? currentWeatherStatus,
+    ForecastDaysStatus? forecastDaysStatus,
+    WeatherInformationStatus? informationStatus,
+    MainDataStatus? mainDataStatus,
+  }) {
+    return HomeState(
+      mainDataStatus: mainDataStatus ?? this.mainDataStatus,
+      currentWeatherStatus: currentWeatherStatus ?? this.currentWeatherStatus,
+      forecastDaysStatus: forecastDaysStatus ?? this.forecastDaysStatus,
+      informationStatus: informationStatus ?? this.informationStatus,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        currentWeatherStatus,
+        forecastDaysStatus,
+        informationStatus,
+        mainDataStatus,
+      ];
 }
