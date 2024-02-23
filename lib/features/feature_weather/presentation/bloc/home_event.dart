a189b75f99a7d24e782dc2843cb8eea014785d35
Mahdi <mti.safdari@gmail.com>
2024-02-23 07:38:04 +0330

    feat(weather): create home event

--- lib/features/feature_weather/presentation/bloc/home_event.dart
+++ lib/features/feature_weather/presentation/bloc/home_event.dart
@@ -1,5 +1,36 @@
 part of 'home_bloc.dart';
 
-abstract class HomeEvent extends Equatable {
-  const HomeEvent();
+abstract class HomeEvent extends Equatable {}
+
+class CurrentWeatherEvent extends HomeEvent {
+  final String cityName;
+  CurrentWeatherEvent(this.cityName);
+
+  @override
+  List<Object?> get props => [cityName];
+}
+
+class ForecastDaysEvent extends HomeEvent {
+  final String cityName;
+  ForecastDaysEvent(this.cityName);
+
+  @override
+  List<Object?> get props => [cityName];
+}
+
+class WeatherInformationEvent extends HomeEvent {
+  final LocationParams locationParams;
+  WeatherInformationEvent(this.locationParams);
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class MainDataEvent extends HomeEvent {
+  final LocationParams params;
+
+  MainDataEvent(this.params);
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
 }
