5a8f167a5138d69ef967c961320334a293fc92fa
Mahdi <mti.safdari@gmail.com>
2024-02-23 07:35:58 +0330

    feat(weather): create get information usecase

--- lib/features/feature_weather/domain/usecases/get_weather_information_usecase.dart
+++ lib/features/feature_weather/domain/usecases/get_weather_information_usecase.dart
@@ -1,12 +1,12 @@
-import 'package:breezehub/core/params/params.dart';
+import 'package:breezehub/core/params/location_params.dart';
 import 'package:breezehub/core/resources/data_state.dart';
 import 'package:breezehub/core/usecases/use_case.dart';
 import 'package:breezehub/features/feature_weather/domain/entities/weather_information_entity.dart';
 import 'package:breezehub/features/feature_weather/domain/repositories/weather_repository.dart';
 
-class GetWeatherInformation extends UseCase<DataState<WeatherInformationEntity>, LocationParams> {
+class GetWeatherInformationUseCase extends UseCase<DataState<WeatherInformationEntity>, LocationParams> {
   final WeatherRepository weatherRepository;
-  GetWeatherInformation(this.weatherRepository);
+  GetWeatherInformationUseCase(this.weatherRepository);
 
   @override
   Future<DataState<WeatherInformationEntity>> call(LocationParams param) {
